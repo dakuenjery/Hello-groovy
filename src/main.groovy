357f36ddcc3a5872f4e1f70528e97cb6fc60ab37
Николай <dakuenjery@gmail.com>
2015-05-26 22:27:09 +0300

    Regexp

--- src/main.groovy
+++ src/main.groovy
@@ -1,27 +1,115 @@
+import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.JTextField
+import java.awt.AlphaComposite
 import java.awt.BorderLayout
 import java.awt.Color
+import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Graphics2D
+import java.awt.GridLayout
 import java.awt.Polygon
 import java.awt.RenderingHints
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.awt.geom.Point2D
 import javax.swing.Timer
-import java.util.function.Function
+import java.awt.image.BufferedImage
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 /**
  * Created by dakue_000 on 20.05.2015.
  */
 
+class Parser {
+    private vars = new String[2]
+    private func = null
+
+    private rpn = new RPN()
+    private class RPN {
+        Deque<String> deque = new ArrayDeque<String>()
+        List<String> list = new ArrayList<String>()
+
+    }
+
+    private class Token {
+        String str
+        Token(str) {
+            this.str = str
+        }
+    }
+
+    static private class Patterns {
+        static Pattern body = ~/^y ?= ?(.*)/
+        static Pattern plus = ~/^(.{1,}?) ?([+-].*)/
+        static Pattern mul = ~/^(.{1,}?) ?([*\\/].*)/
+    }
+
+    Parser(str) {
+        Matcher matcher = str =~ Patterns.body; matcher.find()
+        str = matcher.group(1)
+
+        def plusTokens = parse(str)
+        println plusTokens
+        for (def token in plusTokens) {
+            def mulTokens = parse(token, Patterns.mul)
+
+            println '    ' + mulTokens
+            for (def mulToken in mulTokens) {
+
+            }
+        }
+
+        println plusTokens
+    }
+
+    def getFunc() { return func }
+
+    private parse(str, pattern = Patterns.plus) {
+        def strs = []
+
+        try {
+            for (boolean needNextIteration = true; needNextIteration && str.size(); ) {
+                def matcher = str =~ pattern; matcher.find()
+
+                if (matcher.groupCount() == 2) {
+                    str = matcher.group(2)
+//                    strs << matcher.group(2) + matcher.group(3)
+                    strs << matcher.group(1)
+                }
+//                else {
+//                    needNextIteration = false
+//                    println 'needNextIteration = false'
+//                }
+            }
+        } catch (IllegalStateException e) {
+            strs << str
+        }
+
+        return strs
+    }
+
+    private createFunc(Matcher matcher) {
+        Double d = 1
+        try {
+            d = Double.parseDouble(matcher.group(2))
+        } catch (Exception e) {}
+
+        if (matcher.group(3) == 'x') {
+            return {x -> d*x}
+        } else {
+            throw new Exception('Parse error')
+        }
+    }
+}
 
 class Func {
     static final double ITERATION = 0.01
     private double MAX, startPoint, endPoint
 
-    private int currentAccuracy = 0
+    private int currentAccuracy = 0, maxAccuracy = 10000
     private List<Point2D.Double[]> pointsList = []
     private List<Double> maxList = []
 
@@ -30,44 +118,44 @@ class Func {
     def getMAX() { return maxList[maxList.size()-1] }
     def getPoints(k) { return pointsList[k-1] }
     def accuracy() { return currentAccuracy }
+    def maxAccuracy() { return maxAccuracy }
 
     Func(closure, startPoint, endPoint) {
         this.closure = closure
         this.startPoint = startPoint
         this.endPoint = endPoint
 
-
         initPoints(1)
     }
 
     def initPoints(k) {
         assert(k>0)
+        --k
 
-        if (pointsList.size() == k)
+        if (pointsList.size() == k+1 || pointsList.size() >= maxAccuracy)
             return
 
-        if ( (k-pointsList.size()) > 1)
+        if ( (k-pointsList.size()) >= 1)
             initPoints(k-1)
 
         final int count = (endPoint - startPoint) / ITERATION
         def points = new Point2D.Double[count]
-        def prevPoints = (k > 1) ? pointsList[k-2] : null
+        def prevPoints = (k > 0) ? pointsList[k-1] : null
         def start = startPoint
 
-        MAX = closure(start, k)
+        MAX = closure(start, k+1)
 
         for (int i = 0; i < count; ++i) {
             double prevFuncValue = prevPoints ?  prevPoints[i].y : 0
-            points[i] = new Point2D.Double(start, closure(start, k) + prevFuncValue)
+            points[i] = new Point2D.Double(start, closure(start, k+1) + prevFuncValue)
             if (points[i].y > MAX)
                 MAX = points[i].y
             start += ITERATION
         }
 
-        pointsList[k-1] = points
-        maxList[k-1] = MAX
-        if (currentAccuracy < k)
-            currentAccuracy = k
+        pointsList[k] = points
+        maxList[k] = MAX
+        currentAccuracy = pointsList.size()
     }
 
     /**
@@ -83,11 +171,18 @@ class Func {
     def closure
 }
 
+
+
 class App extends JFrame {
     private Func func
 
     private JPanel panel = new JPanel(new BorderLayout())
-    private JPanel upPanel = new JPanel()
+    private JTextField  textField = new JTextField()
+    private JButton btn = new JButton('Button')
+    private JPanel upPanel = new JPanel(new GridLayout(1, 2)) {{
+        add(textField); add(btn)
+    }}
+
     private Canvas canvas
 
     private Timer timer = new Timer(100, new ActionListener() {
@@ -97,12 +192,10 @@ class App extends JFrame {
             func.initPoints(k++)
             canvas.repaint()
 
-            timer.setDelay( (int)timer.getDelay() * 0.9 )
-            if (k > 500)
+            def time = timer.getDelay()
+            timer.setDelay( (int) (time > 10) ? time*0.9 : 10 )
+            if (k > 5000)
                 timer.stop()
-
-//            k = ((double)k)*1.1
-//            println 'DELAY: ' + timer.getDelay() + '; k: ' + k
         }
     })
 
@@ -119,6 +212,8 @@ class App extends JFrame {
 
             paintLines(g2d)
             drawFuncPolygon(g2d)
+
+            drawBorder(g2d)
         }
 
         private def setupRender(g2d) {
@@ -172,16 +267,16 @@ class App extends JFrame {
 
         private def drawFuncPolygon(g) {
             def color = new Color(0,0,255, 255)
-            int acc = func.accuracy()
+            int acc = func.accuracy(), end = (func.maxAccuracy() >= 10) ? 10 : func.maxAccuracy()
 
-            for (int i = 1; i <= 10; ++i) {
-                int k = (int)0.1*i*acc
+            for (int i = 1; i <= end; ++i) {
+                int k = (int) 1.0/end *i*acc
                 if (!k)
                     continue
 
                 Polygon p = new Polygon()
 
-                g.setColor(new Color(0,0,255, (int)0.1*i*255))
+                g.setColor(new Color(0,0,255, (int)1.0/end*i*255))
 
                 func.forEach({ point ->
                     int x = point.x * MX + X0
@@ -191,6 +286,24 @@ class App extends JFrame {
 
                 g.drawPolyline(p.xpoints, p.ypoints, p.npoints)
             }
+        }
+
+        private def drawBorder(g) {
+            AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.DST_OUT, 1.0)
+            BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB)
+            Graphics2D gImg = img.createGraphics()
+            setupRender(gImg)
+
+            gImg.setPaint(Color.DARK_GRAY)
+            gImg.fillRect(0, 0, getWidth(), getHeight())
+
+            gImg.setComposite(ac)
+
+            gImg.setPaint(Color.WHITE)
+            gImg.fillRoundRect(4, 5, getWidth()-8, getHeight()-10, 5, 5)
+
+            g.drawImage(img, 0, 0, null)
+            gImg.dispose()
         }
 
         private def computConsts() {
@@ -208,14 +321,26 @@ class App extends JFrame {
 
         panel.add(upPanel, BorderLayout.NORTH)
         panel.add(canvas, BorderLayout.CENTER)
+
+//        upPanel.add(textField)
         setContentPane(panel)
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         setBounds(0, 0, 640, 480)
         setVisible(true)
     }
 }
-func = new Func({x, k ->
-    return (Math.pow(-1, k+1)*2/k)*Math.sin(k*x)
-}, -Math.PI*1.2, Math.PI*1.2)
 
-app = new App(func)
+parser = new Parser('y = -3*x - 5/x + 7/x + 9*x*2 - 11 + 13*x + 15 + 17 - 19/x')
+parser2 = new Parser('y = -x*32')
+
+
+//println 'Parser text: f(5): ' + parser.getFunc()(5)
+//
+//func = new Func({x, k ->
+//    return (Math.pow(-1, k+1)*2/k)*Math.sin(k*x)
+//}, -Math.PI*1.2, Math.PI*1.2)
+//
+////func = new Func( {x, k ->
+////    return parser.getFunc()(x) }, -Math.PI*1.2, Math.PI*1.2)
+//
+//app = new App(func)
